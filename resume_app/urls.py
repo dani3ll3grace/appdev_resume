de5b8fc35aa3cdd2328af02beae3a45b00da1d0d
dani3113grace <202180186@palawan.edu.ph>
2024-08-20 00:26:29 +0800

    Cleaned and Finalized

--- resume_app/urls.py
+++ resume_app/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('work/', views.work, name='work'),
     path('about/', views.about, name='about'),
-    path('portfolio/', views.portfolio, name='portfolio'),
     path('project/<int:pk>/', views.project_detail, name='project_detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- resume_app/views.py
+++ resume_app/views.py
@@ -13,10 +13,6 @@ def about(request):
     print("About view called")
     return render(request, 'resume_app/about.html')
 
-def portfolio(request):
-    print("Portfolio view called")
-    return render(request, 'resume_app/portfolio.html')
-
 def home(request):
     projects = Project.objects.all()
     return render(request, 'resume_app/home.html', {'projects': projects})
